d18bd920f6ec5538e90c8d58ee7d992c4a0537ec
Quaad <ajwood1998@hotmail.co.uk>
2025-01-26 17:50:02 +0000

    added link to create page and added routing to it. Create the post request to store a new job and updated layout

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,27 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', [
-        'jobs' => Job::with('employer')->paginate(3)
+    return view('jobs.index', [
+        'jobs' => Job::with('employer')->latest()->paginate(3)
     ]);
 });
 
+Route::post('/jobs', function () {
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
+});
+
+Route::get('/jobs/create', function() {
+    return view('jobs.create');
+});
+
 Route::get('/jobs/{id}', function ($id) {
-    return view('job', [
+    return view('jobs.show', [
         'job' => Job::find($id)
     ]);
 });
